4f2e207dc86fd27188d4e13530bffd7f5a8878e1
Tzienom <tzienom@gmail.com>
2025-07-05 09:04:13 +0100

    Update: add DOMContentLoaded to document object

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
   const usernameInput = document.getElementById("username");
